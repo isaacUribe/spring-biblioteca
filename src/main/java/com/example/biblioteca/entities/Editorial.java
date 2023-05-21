6ccab0163bd1f0fdb9f920649e20d46875c5a7d4
isaacUribe <iuribear@cesde.net>
2023-05-21 18:09:08 -0500

    se crea entiti editorial

---
+++ src/main/java/com/example/biblioteca/entities/Editorial.java
@@ -0,0 +1,51 @@
+package com.example.biblioteca.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
+
+@Entity
+@Table(name = "editorial")
+public class Editorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nombre", unique = true, length = 30)
+    @Size(min = 2)
+    private String nombre;
+    @Column(name = "descripcion", length = 300)
+    private String descripcion;
+
+    public Editorial() {
+    }
+
+    public Editorial(Integer id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
